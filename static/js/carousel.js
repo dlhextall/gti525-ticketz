979bfed37d9482819516435295163f4b43cbdbbc
dlhextall <david.lafrance.17@gmail.com>
2014-05-13 23:10:28 -0400

    Un clic sur un 'indicator' met à jour la description.

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -4,6 +4,9 @@ $(function() {
     $("#featuredShows").on("click", ".carousel-control[data-slide]", function(ev) {
         setTimeout(function() { updateFeaturedInfo() }, 1000);
     });
+    $("#featuredShows").on("click", ".carousel-indicators li[data-slide-to]", function(ev) {
+        setTimeout(function() { updateFeaturedInfo() }, 1000);
+    });
 
     function updateFeaturedInfo() {
         $("#featuredInfo h1").html($("#featuredShows .carousel-inner .active h1").html());
