b5b7ec3255b2499d8836a86a74c226ace91714ec
plourdealix <bonbonbonjarome@gmail.com>
2014-06-14 13:58:50 -0400

    CheckoutController.java et checkout.jsp + quelque modif

---
+++ spring-mvc/src/main/java/ca/etsmtl/ticketz/controller/CheckoutController.java
@@ -0,0 +1,32 @@
+package ca.etsmtl.ticketz.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class CheckoutController {
+	
+	
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/checkout", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		
+		return "checkout";
+	}
+	
+}
