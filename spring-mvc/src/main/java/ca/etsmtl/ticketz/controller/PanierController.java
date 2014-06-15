3ec505c706118f94a6ac5b0a8ffd89065f72fd6b
ez1820 <edgarson.romeus.1@ens.etsmtl.ca>
2014-06-15 11:01:19 -0400

    PanierController in progress

--- spring-mvc/src/main/java/ca/etsmtl/ticketz/controller/PanierController.java
+++ spring-mvc/src/main/java/ca/etsmtl/ticketz/controller/PanierController.java
@@ -1,5 +1,17 @@
 package ca.etsmtl.ticketz.controller;
 
-public class PanierController {
+import java.util.Locale;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class PanierController{
+
+	@RequestMapping(value = "/panier", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		
+		return "panier";
+	}
 }
