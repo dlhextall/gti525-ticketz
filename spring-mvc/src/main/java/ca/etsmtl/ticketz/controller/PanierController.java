a5ad7e53dbe237f830505857f9a55b5f19fbb158
ez1820 <edgarson.romeus.1@ens.etsmtl.ca>
2014-06-15 11:29:21 -0400

    panierController fix

--- spring-mvc/src/main/java/ca/etsmtl/ticketz/controller/PanierController.java
+++ spring-mvc/src/main/java/ca/etsmtl/ticketz/controller/PanierController.java
@@ -1,8 +1,6 @@
 package ca.etsmtl.ticketz.controller;
 
 import java.util.Locale;
-<<<<<<< HEAD
-=======
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,27 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class PanierController {	
+public class PanierController {
+	
 
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/panier", method = RequestMethod.GET)
-	public String panier(Locale locale, Model model) {		
+	public String checkout(Locale locale, Model model) {		
 		return "Panier";
 	}
->>>>>>> FETCH_HEAD
 
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-public class PanierController{
-
-	@RequestMapping(value = "/panier", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		
-		
-		return "panier";
-	}
 }
