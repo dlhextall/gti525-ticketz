b22bddecc0a6a0cc0b0a40d65dbffe455719ddbb
plourdealix <bonbonbonjarome@gmail.com>
2014-06-15 10:57:43 -0400

    Fix Panier.jsp + panier controller

--- spring-mvc/src/main/java/ca/etsmtl/ticketz/controller/PanierController.java
+++ spring-mvc/src/main/java/ca/etsmtl/ticketz/controller/PanierController.java
@@ -1,5 +1,22 @@
 package ca.etsmtl.ticketz.controller;
 
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class PanierController {
+	
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/panier", method = RequestMethod.GET)
+	public String checkout(Locale locale, Model model) {		
+		return "Panier";
+	}
 
 }
