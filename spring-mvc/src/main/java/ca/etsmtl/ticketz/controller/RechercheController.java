d376d9138b907dc3869bf87ec7374ee78586755d
ez1820 <edgarson.romeus.1@ens.etsmtl.ca>
2014-06-16 15:52:17 -0400

    minor Fix on RechercheController

--- spring-mvc/src/main/java/ca/etsmtl/ticketz/controller/RechercheController.java
+++ spring-mvc/src/main/java/ca/etsmtl/ticketz/controller/RechercheController.java
@@ -2,10 +2,12 @@ package ca.etsmtl.ticketz.controller;
 
 import java.util.Locale;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class RechercheController {
 	/**
 	 * Simply selects the home view to render by returning its name.
