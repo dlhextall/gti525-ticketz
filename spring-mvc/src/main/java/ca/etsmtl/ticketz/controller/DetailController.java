81789e48fd9eabc60935c614ba316b931b2632f6
dlhextall <david.lafrance.17@gmail.com>
2014-06-17 02:06:20 -0400

    used AbstractController + added redirect if show id is null

--- spring-mvc/src/main/java/ca/etsmtl/ticketz/controller/DetailController.java
+++ spring-mvc/src/main/java/ca/etsmtl/ticketz/controller/DetailController.java
@@ -3,25 +3,41 @@ package ca.etsmtl.ticketz.controller;
 import java.util.ArrayList;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
 
 import ca.etsmtl.ticketz.dao.TicketzProvider;
 import ca.etsmtl.ticketz.model.Show;
 
 
 @Controller
-public class DetailController {
+public class DetailController extends AbstractController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/detail", method = RequestMethod.GET)
-	public String detail(@RequestParam("id") String id,Locale locale, Model model) {
-		Show spectacle = TicketzProvider.getShowAt(Integer.parseInt(id));
-		model.addAttribute("spectacle",spectacle);
-		return "Detail";
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest _req, HttpServletResponse _resp) throws Exception {
+		ModelAndView model;
+		String idStr = _req.getParameter("id");
+		try {
+			model = new ModelAndView("Detail");
+			int id = Integer.parseInt(idStr);
+			model.addObject("spectacle", TicketzProvider.getShowAt(id));
+			return model;
+		} catch (NumberFormatException _e) {
+			model = new ModelAndView("redirect:/");
+			model.addObject("spectacles", TicketzProvider.getFeaturedShows());
+			return model;
+		}
 	}
 }
