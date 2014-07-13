e3dcdd8e109cd302eb02122c44eb83a9ea2e82a6
plourdealix <bonbonbonjarome@gmail.com>
2014-07-13 15:37:17 -0400

    jquery $post

--- spring-mvc/src/main/webapp/resources/js/detail.js
+++ spring-mvc/src/main/webapp/resources/js/detail.js
@@ -8,14 +8,21 @@ $('#form_btn').click(function() {
 	var body = $("html, body");
 	body.animate({scrollTop:0}, '500', 'swing', function() { 
 		$('#panier').html('Panier (+1)');
+		ajaxForm();
 	});
 });
 
 
 
 function ajaxForm(){
-	
-	
+	$.post("/add",
+			  {
+			    name:"Donald Duck",
+			    city:"Duckburg"
+			  },
+			  function(data,status){
+			    alert("Data: " + data + "\nStatus: " + status);
+			  });
 }
 
 
