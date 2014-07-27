da124cc33e1f5ee4be6583eb13c6fe8885b91309
plourdealix <bonbonbonjarome@gmail.com>
2014-07-27 12:22:46 -0400

    Fixed pour vrai duplication

--- spring-mvc/src/main/webapp/resources/js/detail.js
+++ spring-mvc/src/main/webapp/resources/js/detail.js
@@ -3,32 +3,37 @@ $(document).ready(function() {
 $montant = $("#totalPrice").val();	
 $montant = parseInt($montant,10);
 	
-$( "#nbTickets" ).change(function() {
-	
-	$traitement = parseInt($("#nbTickets").val(), 10)*$montant;
-		
-		$("#totalPrice").val($traitement);
-	});
+$( "#nbTickets" ).change(function() {	
+	$traitement = parseInt($("#nbTickets").val(),10)*$montant;		
+	$("#totalPrice").val($traitement);
+});
 	
 	
-$('#form_btn').click(function() {
+$('#form_btn').click(function() {	
 	
 	var body = $("html, body");
-	body.animate({scrollTop:0}, '500', 'swing', function() { 
+	body.animate({scrollTop:0}, '500', 'swing').promise().done(function(){
 		$('#panier').html('Panier (+1)');		
 		ajaxForm();
-	});
+	});	
+
 });
+
+
 
 
 function ajaxForm(){
 	$nbTicket = parseInt($("#nbTickets").val());
 	$nomSpectacle = $("#nomSpectacle").val();
-	$unityPrice = $("#unityPrice").val();
+	$unityPrice = parseInt($("#unityPrice").val(),10);
 	var idSpectacle = $("#idSpectacle").val();
+	
+	
 	
 	$.get( "panier/add", { nbTickets: $nbTicket, unityPrice: $unityPrice , nomSpectacle: $nomSpectacle, idSpectacle: idSpectacle } )
 	  .done(function( data ) {
+		  
+		  
 		  
 	    
 	  }); 	
