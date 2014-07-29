691392d325c7d163220e4993353c02caeded4894
dlhextall <david.lafrance.17@gmail.com>
2014-07-28 20:30:05 -0400

    search results optimisation

--- spring-mvc/src/main/java/ca/etsmtl/ticketz/controller/SearchResultController.java
+++ spring-mvc/src/main/java/ca/etsmtl/ticketz/controller/SearchResultController.java
@@ -1,6 +1,6 @@
 package ca.etsmtl.ticketz.controller;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,26 +8,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import ca.etsmtl.ticketz.model.Show;
 import ca.etsmtl.ticketz.service.TicketzProvider;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 
 @Controller
 public class SearchResultController {
 	
 	@Autowired
-	TicketzProvider provider = null;
+	private TicketzProvider provider = null;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/searchResult", method = RequestMethod.GET)
-	public String detail(Locale locale, Model model) {		
-		model.addAttribute("spectacles", provider.getAllShows());
+	public String detail(Locale locale, Model model) {	
+		List<Show> shows = (List<Show>) provider.getAllShows();
+		model.addAttribute("spectacles", shows);
 		return "SearchResult";
 	}
 }
