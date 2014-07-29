2849ec9c30c330883a3a942e23e14792ec7f349e
dlhextall <david.lafrance.17@gmail.com>
2014-07-29 18:02:18 -0400

    stub now returns correct message for validation

--- spring-mvc/src/main/java/ca/etsmtl/ticketz/service/PaiementStub.java
+++ spring-mvc/src/main/java/ca/etsmtl/ticketz/service/PaiementStub.java
@@ -1,29 +1,32 @@
 package ca.etsmtl.ticketz.service;
 
+import org.apache.log4j.Logger;
+
 import gti525.paiement.IPaiementDAO;
 import gti525.paiement.InformationsPaiementTO;
 import gti525.paiement.ReponseSystemePaiementTO;
 import gti525.paiement.RequeteAuthorisationTO;
 
 public class PaiementStub implements IPaiementDAO {
+	
+	private static final Logger logger = Logger.getLogger(PaiementStub.class);
 
 	@Override
-	public ReponseSystemePaiementTO approuverTransaction(
-			RequeteAuthorisationTO arg0) {
+	public ReponseSystemePaiementTO approuverTransaction(RequeteAuthorisationTO _req) {
 		ReponseSystemePaiementTO reponse = new ReponseSystemePaiementTO();
 		reponse.setCode(200);
-		reponse.setMessage("Transaction approuvée");
-		reponse.setTransactionId((int) arg0.getTransaction_id());
+		reponse.setMessage("Completed");
+		reponse.setTransactionId((int) _req.getTransaction_id());
 		
 		return reponse;
 	}
 
 	@Override
-	public ReponseSystemePaiementTO effectuerPreauthorisation(
-			InformationsPaiementTO arg0) {
+	public ReponseSystemePaiementTO effectuerPreauthorisation(InformationsPaiementTO _info) {
 		ReponseSystemePaiementTO reponse = new ReponseSystemePaiementTO();
 		reponse.setCode(200);
-		reponse.setMessage("Transaction préapprouvée");
+		reponse.setMessage("Approved");
+		reponse.setTransactionId(1);
 		
 		return reponse;
 	}
