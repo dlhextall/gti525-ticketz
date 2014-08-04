4f8251f5d0997c1dabb4555083ee55a75765c06a
dlhextall <david.lafrance.17@gmail.com>
2014-08-04 19:21:05 -0400

    code cleaning

--- spring-mvc/src/main/java/ca/etsmtl/ticketz/controller/HomeController.java
+++ spring-mvc/src/main/java/ca/etsmtl/ticketz/controller/HomeController.java
@@ -1,91 +1,35 @@
 package ca.etsmtl.ticketz.controller;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
-import ca.etsmtl.ticketz.model.Panier;
-import ca.etsmtl.ticketz.model.Show;
 import ca.etsmtl.ticketz.service.IShowService;
-import ca.etsmtl.ticketz.service.TicketzProvider;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController extends AbstractController {
-	
+
+	private static final Logger LOGGER = Logger.getLogger(HomeController.class);
 	@Autowired
 	private IShowService provider;
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	HttpSession session;
-	public HttpSession getSession() {
-		return session;
-	}
-	public void setSession(HttpSession session) {
-		this.session = session;
-	}
-	Timer timer = new Timer();
 	
-	
-	private final int MINUTES=20;
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-
 	@Override
 	protected ModelAndView handleRequestInternal(HttpServletRequest _req, HttpServletResponse _resp) throws Exception {
 		ModelAndView model = new ModelAndView("home");
-		setSession(_req.getSession());
-		//Panier panier = (Panier)session.getAttribute("panier");
-		 //panier= new Panier();
-		//session.setAttribute("panier", panier); 
 		model.addObject("spectacles", provider.getFeaturedShows());
-		
-		/*TimerTask task = new TimerTask() {
-			
-			  public void run() {
-				  session.invalidate();
-				 System.out.println("Global Timer Current time is :: "+ new Date());
-			  }};
-			  
-			  
-			  timer.scheduleAtFixedRate(task, 0, 1200000);*/
-			  //session.invalidate();
 		return model;
 	}
-	
-
-	/*@Scheduled(fixedDelay = 5000)
-    public void demoServiceMethod()
-    {
-       //System.out.println("Method executed at every 5 seconds. Current time is :: "+ new Date());
-		Panier panier = (Panier)request.getSession().getAttribute("panier");
-		panier= new Panier();
-		request.getSession().setAttribute("panier", panier);
-		System.out.println("Global Timer Current time is :: "+ new Date());
-    }*/
-	
-	      
-	
 	
 }
--- spring-mvc/src/main/java/ca/etsmtl/ticketz/controller/RechercheController.java
+++ spring-mvc/src/main/java/ca/etsmtl/ticketz/controller/RechercheController.java
@@ -1,20 +1,16 @@
 package ca.etsmtl.ticketz.controller;
 
-import java.util.Locale;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
 import ca.etsmtl.ticketz.service.IShowService;
-import ca.etsmtl.ticketz.service.TicketzProvider;
 
 @Controller
 public class RechercheController extends AbstractController {
@@ -26,7 +22,6 @@ public class RechercheController extends AbstractController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/recherche", method = RequestMethod.GET)
-	
 	@Override
 	protected ModelAndView handleRequestInternal(HttpServletRequest _req, HttpServletResponse _resp) throws Exception {
 		ModelAndView model;
